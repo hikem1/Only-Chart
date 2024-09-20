e6d926edf8b118aa6d77754ae7d02c28e8ab6aab
Rouchon Fabien <hikem1@hotmail.fr>
2024-09-20 12:02:48 +0200

    add favorite modal

---
+++ app/partial/favorite_modal.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="favoriteModal" tabindex="-1" aria-labelledby="favoriteModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable modal-lg">
+    <div class="modal-content">
+      <div class="modal-header px-3 py-2">
+        <h5 class="modal-title mx-3 my-1" id="favoriteModalLabel">Favorites : </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body pb-2">
+        <div class="d-flex border-bottom fw-bold">
+          <p class="d-flex w-25 mx-3">Name :</p>
+          <p class="d-flex w-25">Code :</p>
+          <p class="d-flex">Place :</p>
+        </div>
+        <ul id="favorites-list">
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
